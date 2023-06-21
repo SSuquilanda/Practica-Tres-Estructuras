6ad95c3771fca7d0d2c3ba3213c6a966f3d64cb3
Samantha Suquilanda Quilli <129775389+SSuquilanda@users.noreply.github.com>
2023-06-21 18:15:47 -0500

    PracticaTresEstructura

--- NetBeansProjects/PracticaTresEstructura/src/main/java/ec/edu/ups/practicatresestructura/PracticaTresEstructura.java
+++ NetBeansProjects/PracticaTresEstructura/src/main/java/ec/edu/ups/practicatresestructura/PracticaTresEstructura.java
@@ -14,8 +14,67 @@ public class PracticaTresEstructura {
     private Scanner teclado;
     
     public static void main(String[] args) {
-        Scanner teclado = new Scanner (System.in);
-        System.out.println("Ingrese una cadena de texto de signos: ");
-                
+        Scanner teclado = new Scanner  (System.in);
+        Character d = null;
+        String sig;
+        ArrayList<Character> listaCaracteres = new ArrayList<>();
+        ArrayList<Character> finales = new ArrayList<>();
+        System.out.print("Ingresa una lista de signos (separados por espacios): ");
+        String input = teclado.nextLine();
+        String[] caracteres = input.split(" ");
+
+        for (String caracter : caracteres) {
+            char c = caracter.charAt(0);
+            listaCaracteres.add(c);
+        }
+        
+        
+        for (Character caracter1 : listaCaracteres) {
+            String caracter = caracter1.toString();
+            if (caracter.equals("{")) {
+                finales.add(caracter1);
+            } else if (caracter.equals("[")) {
+                finales.add(caracter1);
+            } else if (caracter.equals("(")) {
+                finales.add(caracter1);
+            } else if (caracter.equals("}")) {
+                d = finales.get(finales.size() - 1);
+                sig = d.toString();
+                if (sig.equals("{")) {
+                    finales.remove(finales.size() - 1);
+                } else if (sig.equals("[")) {
+                    finales.add(caracter1);
+                } else if (sig.equals("(")) {
+                    finales.add(caracter1);
+                }
+            } else if (caracter.equals("]")) {
+                d = finales.get(finales.size() - 1);
+                sig = d.toString();
+                if (sig.equals("{")) {
+                    finales.add(caracter1);
+                } else if (sig.equals("[")) {
+                    finales.remove(finales.size() - 1);
+                } else if (sig.equals("(")) {
+                    finales.add(caracter1);
+                }
+            } else if (caracter.equals(")")) {
+                d = finales.get(finales.size() - 1);
+                sig = d.toString();
+                if (sig.equals("{")) {
+                    finales.add(caracter1);
+                } else if (sig.equals("[")) {
+                    finales.add(caracter1);
+                } else if (sig.equals("(")) {
+                    finales.remove(finales.size() - 1);
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (Character elemento : finales) {
+            sb.append(elemento);
+            System.out.println(sb);
+        }
     }
+             
+    
 }
