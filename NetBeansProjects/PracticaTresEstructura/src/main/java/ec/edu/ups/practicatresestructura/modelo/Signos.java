6e5440ccdc343294ebf52b790b9bf4b734e8b9c3
Samantha Suquilanda Quilli <129775389+SSuquilanda@users.noreply.github.com>
2023-06-21 18:16:23 -0500

    Update Signos.java

--- NetBeansProjects/PracticaTresEstructura/src/main/java/ec/edu/ups/practicatresestructura/modelo/Signos.java
+++ NetBeansProjects/PracticaTresEstructura/src/main/java/ec/edu/ups/practicatresestructura/modelo/Signos.java
@@ -15,31 +15,7 @@ import java.util.Stack;
  * @author samanthasuquilandaquilli
  */
 public class Signos {
-    private Scanner teclado;
-    private Stack<String[]>  arregloSignos;
     
-    public Signos() {
-        Scanner teclado = new Scanner (System.in);
-        arregloSignos = new Stack<>();
-    }
-    public String[] ingrsesarCadena() {
-        System.out.print("Ingrese una cadena sin espacios: ");
-        String input = teclado.nextLine();
-        
-        String[] arregloSignos = input.split("");
-        int[] cadena = new int[arregloSignos.length];
-
-        for (int i = 0; i < arregloSignos.length; i++) {
-            arregloSignos[i] = arregloSignos[i].trim();
-        }
-        
-        
-        return arregloSignos;
-    }
-    public void analizarLista (Stack<String[]>  arregloSignos){
-        
-
-}
     }
 
 
